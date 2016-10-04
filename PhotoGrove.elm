3f29a34f618b660802a5df0f84ad2d48550f084e
Abey Onalaja <abey.onalaja@gmail.com>
2016-10-04 13:29:44 +0100

    Add selected thumbnail

--- PhotoGrove.elm
+++ PhotoGrove.elm
@@ -1,16 +1,43 @@
 module PhotoGrove exposing (..)
 
 import Html exposing (..)
-import Html Attributes exposing
+import Html.Attributes exposing (..)
+
+
+initalModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
+
+
+urlPrefix =
+    "http://elm-in-action.com/"
+
 
 view model =
-  div [class "content"]
-  [h1 [] [text "Photo Grove"]
-  , div [id "thumbnails"]
-  [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-  ,[ img [ src "http://elm-in-action.com/2.jpeg" ] []
-  ,[ img [ src "http://elm-in-action.com/3.jpeg" ] []]
-  ]
+    div [ class "content" ]
+        [ h1 [] [ text "Photo Grove" ]
+        , div [ id "thumbnails" ]
+            (List.map (\photo -> viewThumbnail model.selectedUrl photo) model.photos)
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
+        ]
+
+
+viewThumbnail selectedUrl thumbnail =
+    img
+        [ src (urlPrefix ++ thumbnail.url)
+        , classList [ ( "selected", selectedUrl == thumbnail.url ) ]
+        ]
+        []
+
 
 main =
-  view "no model yet"
+    view initalModel
